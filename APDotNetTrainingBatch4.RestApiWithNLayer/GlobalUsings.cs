6638ebc1ada12b99a5905ff3172df178d903ce04
AyePwint49 <ayepwintsa49@gmail.com>
2024-05-17 19:39:09 +0800

    Layered (N-Layer) Architecture

---
+++ APDotNetTrainingBatch4.RestApiWithNLayer/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
+global using APDotNetTrainingBatch4.RestApiWithNLayer.Models;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Mvc;
+
