1e928a51f67077a2f419aa19fe7ccf91f1a9748d
AyePwint49 <ayepwintsa49@gmail.com>
2024-06-11 01:11:09 +0800

    [Complete] Pizza Api

---
+++ DotNetTrainingBbatch4.PizzaApi/ConnectionStrings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APDotNetTrainingBatch4.PizzaApi
+{
+    internal class ConnectionStrings
+    {
+        public static SqlConnectionStringBuilder SqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = ".",
+            InitialCatalog = "DotNetTrainingBatch4",
+            UserID = "sa",
+            Password = "12345",
+            TrustServerCertificate = true
+        };
+    }
+}
