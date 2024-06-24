428c38892f2cea679085322c89d7f3a1d83285b9
AyePwint49 <ayepwintsa49@gmail.com>
2024-06-24 21:04:02 +0800

    [Complete] - Web MVC BlogList

---
+++ APDotNetTrainingBatch4.MvcApp/Controllers/BlogController.cs
@@ -0,0 +1,20 @@
+﻿using APDotNetTrainingBatch4.MvcApp.Db;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APDotNetTrainingBatch4.MvcApp.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly AppDbContext _db;
+        public BlogController(AppDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var lst = await _db.Blogs.ToListAsync();
+            return View(lst);
+        }
+    }
+}
---
+++ APDotNetTrainingBatch4.MvcApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace APDotNetTrainingBatch4.MvcApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
