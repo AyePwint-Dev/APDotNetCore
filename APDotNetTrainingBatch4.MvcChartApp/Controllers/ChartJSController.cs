a2a9908c3d2889c4fc26fd469bfafe4af5fc5398
AyePwint49 <ayepwintsa49@gmail.com>
2024-07-15 20:23:00 +0800

    [Complete] Hight Chart

---
+++ APDotNetTrainingBatch4.MvcChartApp/Controllers/ChartJSController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APDotNetTrainingBatch4.MvcChartApp.Controllers
+{
+    public class ChartJSController : Controller
+    {
+        public IActionResult ExampleChart()
+        {
+            return View();
+        }
+        public IActionResult InterpolationLineChart()
+        {
+            return View();
+        }
+    }
+}
