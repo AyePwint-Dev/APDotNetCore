24236f94f0dd51d0118a4e49507c64993702574b
AyePwint49 <ayepwintsa49@gmail.com>
2024-07-15 20:36:20 +0800

    [Complete] CanvasJs

---
+++ APDotNetTrainingBatch4.MvcChartApp/Controllers/CanvasJsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APDotNetTrainingBatch4.MvcChartApp.Controllers
+{
+    public class CanvasJsController : Controller
+    {
+        public IActionResult LineChart()
+        {
+            return View();
+        }
+        public IActionResult SpLineChart() {
+            return View();
+        }
+
+    }
+}
